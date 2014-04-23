9246c9c1ae585d021ebaafba5d0df840010a31ab
joaochencci <joaochencci@gmail.com>
2014-04-23 02:50:52 -0300

    eating bugfix

--- sushi.c
+++ sushi.c
@@ -66,9 +66,7 @@ void* sushi_bar(void* arg) {
 
 		/* muda o estado do cliente para SITTING */
 		state[client_id] = S;
-		// insert_client(client_id);
-		usleep(TIMER);
-		display_table(client_id);
+		insert_client(client_id);
 
 		/* Muda estado do cliente para COMENDO */
 		state[client_id] = E;
@@ -315,49 +313,11 @@ void display_table(int client_id) {
 
 /*imprime clientes entrando*/
 void insert_client(int client_id) {
-	int i, j, k;      
+	int i;
 
-	if(eating == 1) {
-		/* posicao final do cliente (quanto maior, mais pra esquerda anda) */
-		for(i=0; i<61-n_spaces; i++) {
-			usleep(TIMER);
-			display_table(client_id); 
-
-			/* posicao inicial de entrada (quanto maior, mais longe sai da fila) */
-			for(j=i; j<70; j++) {
-				printf(" ");
-			}
-
-			printf("C\n");
-		}
-		spot[client_id] = j-i+1;
-	}
-	else {
-		/* posicao final do cliente (quanto maior, mais pra esquerda anda) */
-		for(i=0; i<(71-n_spaces*eating-n_spaces); i++) {
-			usleep(TIMER);
-			display_table(client_id); 
-
-			/* posicao inicial do primeiro */
-			for(j=(61-n_spaces); j<70; j++) {
-				printf(" ");
-			}
-			printf("C");
-
-			for(j=2; j<eating; j++) {
-				for(k=0; k<n_spaces; k++) {
-					printf(" ");
-				}
-				printf("C");
-			}
-			/* posicao inicial de entrada (quanto maior, mais longe sai da fila) */
-			for(j=i; j<(70-eating*n_spaces); j++) {
-				printf(" ");
-			}
-
-			printf("C\n");
-		}
-		spot[client_id] = eating*(n_spaces+1) + 9;
+	for(i=0; i<61-n_spaces; i++) {
+		usleep(TIMER);
+		display_table(client_id); 
 	}
 }
 
